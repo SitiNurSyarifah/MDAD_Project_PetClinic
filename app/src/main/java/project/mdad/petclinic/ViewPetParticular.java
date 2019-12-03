85ddaf47c130dcdb23d8cc78f7f22d48c2ff0f23
snf <sitinoorfatinrawi@gmail.com>
2019-12-03 14:38:07 +0800

    view pet particular

---
+++ app/src/main/java/project/mdad/petclinic/ViewPetParticular.java
@@ -0,0 +1,14 @@
+package project.mdad.petclinic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewPetParticular extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pet_particular);
+    }
+}

df70e46a89d98602cd6a46463739da2b89291f68
snf <sitinoorfatinrawi@gmail.com>
2019-12-10 11:49:34 +0800

    BookAppt Java

---
+++ app/src/main/java/project/mdad/petclinic/BookAppt.java
@@ -0,0 +1,14 @@
+package project.mdad.petclinic;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BookAppt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_appt);
+    }
+}

718e8c5df30e6b5b0f589da529fb768bb0342d1d
snf <sitinoorfatinrawi@gmail.com>
2019-12-10 12:23:01 +0800

    out standing bill java

---
+++ app/src/main/java/project/mdad/petclinic/OutstandingBills.java
@@ -0,0 +1,14 @@
+package project.mdad.petclinic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OutstandingBills extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_outstanding_bills);
+    }
+}

8bc72d121a5476edfcb66e74fa0f191bd3bc088d
snf <sitinoorfatinrawi@gmail.com>
2019-12-10 11:28:04 +0800

    EditPet java

---
+++ app/src/main/java/project/mdad/petclinic/EditPetParticular.java
@@ -0,0 +1,14 @@
+package project.mdad.petclinic;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditPetParticular extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_pet_particular);
+    }
+}
